6d6f9973707d6c3ae2acb05e521eb756a6def67c
lluizk <111928188+lluizk@users.noreply.github.com>
2023-05-18 20:36:28 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
 const handleSearch = async (event) => {
   event.preventDefault();
 
-  // implemente a consulta a partir daqui
-
-  //// Exemplo de endpoint: https://api.tvmaze.com/search/shows?q=lost
-
-  //// Elementos de leiaute importantes:
-
-  //  #message: use para exibir mensagens aos usuário, por exemplo:
+  const searchInput = document.querySelector('#search-input');
+  const query = searchInput.value;
 
   const message = document.querySelector('#message');
-  message.innerHTML = 'exercício ainda não resolvido.';
 
-  //  #shows: conterá os shows, cada um em um <li>, por exemplo:
-  // <li>
-  //   <img class="poster" src="https://static.tvmaze.com/uploads/images/medium_portrait/0/1389.jpg" />
-  //   <span class="show-name">Lost</span>
-  // </li>
+  try {
+    const response = await fetch(`https://api.tvmaze.com/search/shows?q=${query}`);
+    const data = await response.json();
+
+    if (data.length > 0) {
+      let html = '';
+
+      for (let i = 0; i < data.length; i++) {
+        const show = data[i].show;
+        html += `<div class="result">
+                   <h2>${show.name}</h2>
+                   <img src="${show.image.medium}" alt="${show.name}">
+                 </div>`;
+      }
+
+
+      const showsContainer = document.createElement('div');
+      showsContainer.id = 'shows';
+      showsContainer.innerHTML = html;
+
+      message.innerHTML = '';
+      message.appendChild(showsContainer);
+    } else {
+      message.innerHTML = 'Not found.';
+    }
+  } catch (error) {
+    message.innerHTML = 'Erro ao buscar informações.';
+    console.error(error);
+  }
 };
 
 document.addEventListener('DOMContentLoaded', () => {
